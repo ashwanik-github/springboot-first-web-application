65ee9fc29d1b97c96173c8e205238c275d5ef16c
Ashwani Kumar <ashwini13522@gmail.com>
2019-04-02 08:31:09 +0530

    this is the second commit to upload the target

---
+++ target/classes/data.sql
@@ -0,0 +1,6 @@
+insert into todo values (100001,"Learn Spring Boot",false,sysdate(),"dummy");
+insert into todo values (100002,'Learn AngularJS',false,sysdate(),'dummy');
+insert into todo values (100003,'Learn Microservices',false,sysdate(),'dummy');
+insert into todo values (100004,'Learn Spring',false,sysdate(),'dummy');
+insert into todo values (100005,'Learn Skating',false,sysdate(),'dummy');
+select * from todo;
